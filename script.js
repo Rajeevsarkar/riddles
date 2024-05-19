bc5490eebfcfa77961ac78ab8c865789289e41c9
Rajeevsarkar <rajeevsarkar27@gmail.com>
2024-05-19 17:23:43 +0530

    added 2 buttons-> 1. share 2. next

--- script.js
+++ script.js
@@ -19,12 +19,60 @@ fetch(apiUrl, requestOptions)
     document.getElementById('q-box').innerHTML += `
     <h5>${data.question}</h5>
     <button onclick="getanswer('${data.answer}')">Get answer</button>
+    <button onclick="nextButton()">Next Riddle</button>
+    <button class="details" id="q-details" onclick="qshare('${data.question.replace(
+      /'/g,
+      "\\'",
+    )}')">share</button>
     `
   })
   .catch((error) => {
     console.error('Error:', error)
   })
 
+function nextButton() {
+  document.getElementById('q-box').innerHTML = ''
+  document.getElementById('answer').innerHTML = ''
+
+  fetch(apiUrl, requestOptions)
+    .then((response) => {
+      return response.json()
+    })
+    .then((data) => {
+      console.log(data)
+      data = data[0]
+      document.getElementById('q-box').innerHTML += `
+    <h5>${data.question}</h5>
+    <button onclick="getanswer('${data.answer}')">Get answer</button>
+    <button onclick="nextButton()">Next Riddle</button>
+    <button class="details" id="q-details" onclick="qshare('${data.question.replace(
+      /'/g,
+      "\\'",
+    )}')">share</button>
+
+    `
+    })
+    .catch((error) => {
+      console.error('Error:', error)
+    })
+}
 function getanswer(data) {
   document.getElementById('answer').innerHTML = data
+}
+
+async function qshare(data) {
+  try {
+    const shareData = {
+      title: 'question',
+      text: data.question,
+    }
+    await navigator.share(shareData)
+  } catch (e) {
+    console.log('error')
+  }
+}
+function escapeHtml(string) {
+  return String(string).replace(/[&<>"'\/]/g, function (s) {
+    return entityMap[s]
+  })
 }
